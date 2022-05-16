cef42cd513bb3006caab50a37cb4dfffdaad2ecc
siangi <siangi@gmx.ch>
2022-05-16 12:24:20 +0200

    added primary and secondary button

---
+++ src/components/buttons/PrimaryButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PrimaryButton(props) {
+  return (
+    <button className='box-border bg-shade_darker_white font-bodyFont text-darkmode_black tracking-wider px-6 py-1' onClick={props.action}>{props.caption}</button>
+  )
+}
+
+export default PrimaryButton
