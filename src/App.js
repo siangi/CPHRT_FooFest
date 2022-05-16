767416dc1339a3f077da2e72777c8f9d5edce347
siangi <siangi@gmx.ch>
2022-05-16 11:41:15 +0200

    Added different Routes for our Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Schedule from "./pages/Schedule";
+import Favorites from "./pages/Favorites";
+import Shop from "./pages/Shop";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1 className="text-3xl font-bold underline">Hello world!</h1>
+                <h1 className="text-3xl font-bold underline">Header!</h1>
             </header>
+            <Routes>
+                <Route path="/" element={<Home></Home>}></Route>
+                <Route path="/schedule" element={<Schedule></Schedule>}></Route>
+                <Route path="/favorites" element={<Favorites></Favorites>}></Route>
+                <Route path="/shop" element={<Shop></Shop>}></Route>
+            </Routes>
         </div>
     );
 }
