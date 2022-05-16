beb9e3ec4482b46f83d0ae286122519cd0a3fbb8
tomineodegard <tomi0105@stud.kea.dk>
2022-05-16 11:47:53 +0200

    setting up colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                darkmode_black: "#0E0E10",
+                darkmode_black2: "#1C1C21",
+                darkmode_black3: "#26262C",
+                darkmode_black4: "#2F2F37",
+                darkmode_black5: "#383842",
+                darkmode_black6: "#42424D",
+                darkmode_black7: "#4B4B58",
+                darkmode_black8: "#545463",
+
+                shade_darker_white: "#F4F4F6",
+
+                accent_yellow: "#E19F54",
+                accent_yellow2: "#E7B174",
+                accent_yellow3: "#EABB85",
+
+                accent_red: "#B85C38",
+                accent_red2: "#C56844",
+                accent_red3: "#CA7553",
+
+                accent_blue: "#448FA3",
+                accent_blue2: "#52A1B7",
+                accent_blue3: "#61A9BD",
+            },
+            fontFamily: {
+                bodyFont: "futura-pt, sans-serif",
+                displayFont: "boucherie-block, sans-serif",
+
+            }
+        },
     },
     plugins: [],
 };
