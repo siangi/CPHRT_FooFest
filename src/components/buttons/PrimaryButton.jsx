fd9939c3b46ab1a937f5cdac9d6129bbffd345a6
siangi <siangi@gmx.ch>
2022-05-16 12:37:51 +0200

    added tertiary button, nicer code fomratting for primary

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function PrimaryButton(props) {
   return (
-    <button className='box-border bg-shade_darker_white font-bodyFont text-darkmode_black tracking-wider px-6 py-1' onClick={props.action}>{props.caption}</button>
+    <button 
+    className='box-border bg-shade_darker_white 
+    font-bodyFont text-darkmode_black tracking-wider 
+    px-6 py-1' 
+    onClick={props.action}>
+        {props.caption}
+    </button>
   )
 }
 
