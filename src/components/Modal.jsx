74665e31200bcbfa1553855b7a97fae19f2fb23f
George <geor2335@stud.kea.dk>
2022-05-19 15:43:05 +0200

    introduced read more

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,27 @@
 import { BandContext } from "../pages/Overview";
-import React from "react";
+import H2 from "../components/typography/H2";
+import P from "../components/typography/P";
+import React, { useState } from "react";
 
 export default function Modal({ modalOpen, setModalOpen }) {
   const bandObj = React.useContext(BandContext);
+  const [isReadMore, setIsReadMore] = useState(true);
 
   return modalOpen ? (
     <section className="fixed z-10 w-full h-full overflow-auto bg-[rgba(0,0,0,0.4)] inset-0">
-      <div className="p-6 bg-darkmode_black2 text-white mx-auto my-[5%] w-[80%] flex">
-        <img src={bandObj.logo} alt={bandObj.name} className="w-[40%]" />
-        {bandObj.name}
-        <button onClick={() => setModalOpen(false)}>close</button>
+      <div className="p-10 lg:p-6 bg-darkmode_black2 text-white mx-auto my-[5%] w-[80%] grid justify-items-center lg:grid-cols-2 gap-6">
+        <img src={bandObj.logo} alt={bandObj.name} className="object-cover" />
+
+        <div className="">
+          <H2>{bandObj.name}</H2>
+          <P>
+            {isReadMore ? bandObj.bio.slice(0, 150) : bandObj.bio}
+            <span onClick={() => setIsReadMore(!isReadMore)}>
+              {isReadMore ? "...read more" : " show less"}
+            </span>
+          </P>
+          <button onClick={() => setModalOpen(false)}>close</button>
+        </div>
       </div>
     </section>
   ) : null;
--- src/components/OverviewCard.jsx
+++ src/components/OverviewCard.jsx
@@ -5,14 +5,20 @@ import { useState } from "react";
 
 export default function OverviewCard({ imgUrl, title, stage, runeUrl }) {
   const [modalOpen, setModalOpen] = useState(false);
+  const [loaded, setLoaded] = useState(false);
 
   return (
     <>
       <button className="h-full w-full" onClick={() => setModalOpen(true)}>
         <figure className="grid h-full w-full">
+          {loaded ? null : (
+            <div className="h-full bg-cyan-400 w-full object-cover  row-start-1 col-start-1" />
+          )}
           <img
-            className="h-full w-full object-cover row-start-0 col-start-0 row-start-1 col-start-1"
+            style={loaded ? {} : { display: "none" }}
+            className="h-full w-full object-cover row-start-1 col-start-1"
             src={imgUrl}
+            onLoad={() => setLoaded(true)}
             alt={`${title} band logo`}
           />
           <CardSticker title={title} stage={stage} runeUrl={runeUrl} />
