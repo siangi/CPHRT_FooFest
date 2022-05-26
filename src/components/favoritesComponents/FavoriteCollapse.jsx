1b8dce0336e682a42d9e6d62b852ee65c30a2236
George <geor2335@stud.kea.dk>
2022-05-26 17:31:44 +0200

    added spotify links

--- src/components/favoritesComponents/FavoriteCollapse.jsx
+++ src/components/favoritesComponents/FavoriteCollapse.jsx
@@ -1,6 +1,88 @@
 import { BsSpotify } from "react-icons/bs";
 
 export default function FavoriteCollapse({ isCollapseOpen, bandObj }) {
+  const spotifyUrls = [
+    {
+      name: "Queen",
+      link: "https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d?si=iLbaiUXXR_qRl7qpJ5PXVQ",
+    },
+
+    {
+      name: "A Perfect Circle",
+      link: "https://open.spotify.com/artist/4DFhHyjvGYa9wxdHUjtDkc?si=FmCjWf46TiGKcP_HbdIpcw",
+    },
+
+    {
+      name: "Tool",
+      link: "https://open.spotify.com/artist/2yEwvVSSSUkcLeSTNyHKh8?si=WJB4cqZfSvGrjXpJB4w2mA",
+    },
+
+    {
+      name: "Terminalist",
+      link: "https://open.spotify.com/artist/5N4HmRQloswgMMTnnIHQmQ?si=GQa0u1cjTZ6W6bO9ZzBSUQ",
+    },
+
+    {
+      name: "Led Zeppelin",
+      link: "https://open.spotify.com/artist/36QJpDe2go2KgaRleHCDTp?si=Eyh8bqOkQc2o9wEJ86g8aA",
+    },
+
+    {
+      name: "The Beatles",
+      link: "https://open.spotify.com/artist/3WrFJ7ztbogyGnTHbHJFl2?si=1NH7PmfkS7GmP4g99C8A8A",
+    },
+
+    {
+      name: "Pink Floyd",
+      link: "https://open.spotify.com/artist/0k17h0D3J5VfsdmQ1iZtE9?si=8fIaWzhOQhCycnpTmdnmTA",
+    },
+
+    {
+      name: "AC/DC",
+      link: "https://open.spotify.com/artist/711MCceyCBcFnzjGY4Q7Un?si=X38H_gxHT4q86FTNR2UUpA",
+    },
+
+    {
+      name: "Metallica",
+      link: "https://open.spotify.com/artist/2ye2Wgw4gimLv2eAKyk1NB?si=i5FrWEKOSkCh3gxCo5LgSg",
+    },
+
+    {
+      name: "The Rolling Stones",
+      link: "https://open.spotify.com/artist/22bE4uQ6baNwSHPVcDxLCe?si=Ijknax2MRD6gGnB2nXWDMA",
+    },
+
+    {
+      name: "Black Sabbath",
+      link: "https://open.spotify.com/artist/22bE4uQ6baNwSHPVcDxLCe?si=Ijknax2MRD6gGnB2nXWDMA",
+    },
+
+    {
+      name: "Guns N' Roses",
+      link: "https://open.spotify.com/artist/3qm84nBOXUEQ2vnTfUTTFC?si=ICun66x6R-qG4FatExts_g",
+    },
+
+    {
+      name: "Nirvana",
+      link: "https://open.spotify.com/artist/6olE6TJLqED3rqDCT0FyPh?si=hobJDKiGRgmnUoORMTX9fw",
+    },
+
+    {
+      name: "The Who",
+      link: "https://open.spotify.com/artist/67ea9eGLXYMsO2eYQRui3w?si=Ymr0d5qQR7-t6j8SFbIqVQ",
+    },
+
+    {
+      name: "Raised Fist",
+      link: "https://open.spotify.com/artist/7ik758oYwkKmQtbZtAdOOm?si=CZ_M7pyXSWODlEVJnMrbpg",
+    },
+
+    {
+      name: "Refused",
+      link: "https://open.spotify.com/artist/5sdxGvwxI1TkTA6Pu2jnTb?si=_hBpoul3Tl6Ul1X-qOtKvQ",
+    },
+  ];
+
   return isCollapseOpen ? (
     <div className=" col-start-1 col-end-7">
       <div
@@ -25,10 +107,20 @@ export default function FavoriteCollapse({ isCollapseOpen, bandObj }) {
           {bandObj.members.join(", ")}
         </p>
         <strong>
-          <a href="" target="_blank" className="flex justify-between mt-6">
-            Listen on Spotify
-            <BsSpotify size="1.5rem" />
-          </a>
+          {spotifyUrls.map(
+            (band) =>
+              band.name === bandObj.name && (
+                <a
+                  href={band.link}
+                  rel="noreferrer"
+                  target="_blank"
+                  className="flex justify-between mt-6"
+                >
+                  Listen on Spotify
+                  <BsSpotify size="1.5rem" />
+                </a>
+              )
+          )}
         </strong>
       </div>
     </div>
