f90a47587a62c79055a2d8f52b63a855f7d76eb5
siangi <siangi@gmx.ch>
2022-05-26 11:53:46 +0200

    didnt commit everything before

--- src/contexts/ShopContext.jsx
+++ src/contexts/ShopContext.jsx
@@ -6,6 +6,7 @@ export const ShopContext = createContext();
 
 export const ShopProvider = ({ children }) => {
     const baseIconPath = process.env.PUBLIC_URL + "/icons/";
+    const baseImagePath = process.env.PUBLIC_URL + "/images/";
     const [shopData, setShopData] = useState({
         tickets: [{
             id: 0,
@@ -22,7 +23,26 @@ export const ShopProvider = ({ children }) => {
             imagePath: baseIconPath + "VIPGold.svg",
             amount: 0,
           }],
-        tents: [],
+        tents: [
+            {
+                id: 0,
+                title:"3 Person Tent",
+                price:"399 Kr.",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui. Tortor posuere ac ut consequat semper viverra.",
+                imagePath: baseImagePath + "3PersonTent.jpg",
+                spaceForPeople: 3,
+                amountOfTents: 0,
+            },
+            {
+                id: 1,
+                title:"2 Person Tent",
+                price:"299 Kr.",
+                description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit massa enim nec dui. Tortor posuere ac ut consequat semper viverra.",
+                imagePath: baseImagePath + "2PersonTent.jpg",
+                spaceForPeople: 2,
+                amountOfTents: 0,
+            }
+        ],
         campground: "",
         greenCamping: {},
         persons: [],
