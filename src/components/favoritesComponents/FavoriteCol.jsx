08d8e6452574741b33d0daebd0453fd252642e80
George <geor2335@stud.kea.dk>
2022-05-29 00:13:06 +0200

    responsive done

--- src/components/favoritesComponents/FavoriteCol.jsx
+++ src/components/favoritesComponents/FavoriteCol.jsx
@@ -3,18 +3,20 @@ import H2 from "../typography/H2";
 import { AllBandsContext } from "../../App";
 import FavoriteBandCard from "./FavoriteBandCard";
 
-export default function FavoriteCol({ day, index, colIndex }) {
+export default function FavoriteCol({ day }) {
   const allBands = React.useContext(AllBandsContext);
 
   return (
-    <article className={` flex flex-col items-center`}>
+    <article
+      className={`row-start-1 row-end-2 col-start-1 col-end-5 flex flex-col items-center`}
+    >
       <H2>{day}</H2>
 
       <div>
         {allBands
           .filter((band) => band.day === day)
-          .map((band) => (
-            <FavoriteBandCard bandObj={band} />
+          .map((band, index) => (
+            <FavoriteBandCard key={index} bandObj={band} />
           ))}
       </div>
     </article>
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import H1 from "../components/typography/H1";
 import FavoriteCol from "../components/favoritesComponents/FavoriteCol";
 import {
   HiOutlineArrowNarrowRight,
   HiOutlineArrowNarrowLeft,
 } from "react-icons/hi";
+import { IconContext } from "react-icons";
 
 export const BandContext = React.createContext();
 
 export default function Favorites() {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [colIndex, setColIndex] = useState(0);
 
   const days = [
@@ -21,28 +23,58 @@ export default function Favorites() {
     "Sunday",
   ];
 
+  useEffect(() => {
+    window.addEventListener("resize", () => setScreenWidth(window.innerWidth));
+  });
+
   return (
     <>
       <H1>Favorites</H1>
 
-      <section className="flex gap-10 items-baseline">
-        <button className={`${colIndex === 0 && "invisible"} text-white`}>
-          <HiOutlineArrowNarrowLeft
-            size="2rem"
-            onClick={() => setColIndex((prev) => prev - 1)}
-          />
-        </button>
+      <section className="grid grid-cols-4 sm:flex gap-10 items-baseline">
+        <IconContext.Provider value={{ size: 45, color: "white" }}>
+          <button
+            className={`${
+              colIndex === 0 && "invisible"
+            } row-start-1 row-end-2 col-start-1 col-end-2 z-10`}
+          >
+            <HiOutlineArrowNarrowLeft
+              onClick={() => setColIndex((prev) => prev - 1)}
+            />
+          </button>
 
-        {days.slice(colIndex, colIndex + 3).map((day, index) => (
-          <FavoriteCol day={day} index={index} colIndex={colIndex} />
-        ))}
+          {days
+            .slice(
+              colIndex,
+              screenWidth > 1000
+                ? colIndex + 3
+                : screenWidth < 800
+                ? colIndex + 1
+                : colIndex + 2
+            )
+            .map((day, index) => (
+              <FavoriteCol key={index} day={day} />
+            ))}
 
-        <button className={`${colIndex === 4 && "invisible"} text-white`}>
-          <HiOutlineArrowNarrowRight
-            size="2rem"
-            onClick={() => setColIndex((prev) => prev + 1)}
-          />
-        </button>
+          <button
+            className={`${
+              (colIndex === 4 || colIndex === 5 || colIndex === 6) &&
+              screenWidth > 1000
+                ? "invisible"
+                : (colIndex === 5 || colIndex === 6) &&
+                  screenWidth > 800 &&
+                  screenWidth < 1000
+                ? "invisible"
+                : colIndex === 6 && screenWidth < 800
+                ? "invisible"
+                : ""
+            } row-start-1 row-end-2 col-start-4 col-end-5 z-10 justify-self-end`}
+          >
+            <HiOutlineArrowNarrowRight
+              onClick={() => setColIndex((prev) => prev + 1)}
+            />
+          </button>
+        </IconContext.Provider>
       </section>
     </>
   );
