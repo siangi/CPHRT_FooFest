e9fe356e9ddc72d534f3cbff29f88fa6fdf792ae
George <geor2335@stud.kea.dk>
2022-05-28 20:26:24 +0200

    fixed arrow bug

--- src/components/favoritesComponents/FavoriteCol.jsx
+++ src/components/favoritesComponents/FavoriteCol.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import H2 from "../typography/H2";
+import { AllBandsContext } from "../../App";
+import FavoriteBandCard from "./FavoriteBandCard";
 
-export default function FavoriteCol({ day }) {
+export default function FavoriteCol({ day, index, colIndex }) {
+  const allBands = React.useContext(AllBandsContext);
+
   return (
-    <>
+    <article className={` flex flex-col items-center`}>
       <H2>{day}</H2>
 
-      <div></div>
-    </>
+      <div>
+        {allBands
+          .filter((band) => band.day === day)
+          .map((band) => (
+            <FavoriteBandCard bandObj={band} />
+          ))}
+      </div>
+    </article>
   );
 }
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,31 +1,48 @@
-import React from "react";
-import { AllBandsContext } from "../App";
+import React, { useState } from "react";
 import H1 from "../components/typography/H1";
 import FavoriteCol from "../components/favoritesComponents/FavoriteCol";
+import {
+  HiOutlineArrowNarrowRight,
+  HiOutlineArrowNarrowLeft,
+} from "react-icons/hi";
 
 export const BandContext = React.createContext();
 
 export default function Favorites() {
-  const allBands = React.useContext(AllBandsContext);
+  const [colIndex, setColIndex] = useState(0);
 
   const days = [
     "Monday",
     "Tuesday",
     "Wednesday",
-    // "Thursday",
-    // "Friday",
-    // "Saturday",
-    // "Sunday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
   ];
 
   return (
     <>
       <H1>Favorites</H1>
 
-      <section className="flex justify-between">
-        {days.map((day) => (
-          <FavoriteCol day={day} />
+      <section className="flex gap-10 items-baseline">
+        <button className={`${colIndex === 0 && "invisible"} text-white`}>
+          <HiOutlineArrowNarrowLeft
+            size="2rem"
+            onClick={() => setColIndex((prev) => prev - 1)}
+          />
+        </button>
+
+        {days.slice(colIndex, colIndex + 3).map((day, index) => (
+          <FavoriteCol day={day} index={index} colIndex={colIndex} />
         ))}
+
+        <button className={`${colIndex === 4 && "invisible"} text-white`}>
+          <HiOutlineArrowNarrowRight
+            size="2rem"
+            onClick={() => setColIndex((prev) => prev + 1)}
+          />
+        </button>
       </section>
     </>
   );
