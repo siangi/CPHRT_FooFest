6ad089b415d94ca873797a26b6c9f6176e621132
siangi <siangi@gmx.ch>
2022-05-31 14:39:47 +0200

    better mobile styling for progress bar and payment radio buttons.

--- src/components/inputs/RadioButton.jsx
+++ src/components/inputs/RadioButton.jsx
@@ -7,11 +7,11 @@ function RadioButton(props) {
         type="radio" 
         id={props.id} 
         name={props.groupName}
-        className="peer relative z-10 bg-darkmode_black2 border-shade_darker_white border-0 checked:bg-accent_yellow active:bg-accent_yellow focus:bg-accent_yellow checked:focus:bg-accent_yellow focus:ring-accent_yellow focus:ring-offset-transparent focus:ring-0"
+        className="peer hidden md:inline-block relative z-10 bg-darkmode_black border-shade_darker_white border-0 checked:bg-accent_yellow active:bg-accent_yellow focus:bg-accent_yellow checked:focus:bg-accent_yellow focus:ring-accent_yellow focus:ring-offset-transparent focus:ring-0"
         onChange={props.onChange}
         checked={props.checked}>      
         </input>
-      <label htmlFor={props.id} className="relative z-0 ml-[-0.5rem] box-border font-bodyFont font-bold w-fit h-fit p-3 pl-5 bg-darkmode_black3 peer-checked:bg-darkmode_black6">
+      <label htmlFor={props.id} className="md:relative md:z-0 md:ml-[-0.5rem] box-border font-bodyFont font-bold w-fit h-fit p-3 md:pl-5 bg-darkmode_black2 peer-checked:bg-darkmode_black6">
           {props.label}
       </label>
     </div>
