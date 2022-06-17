f914619c22493c411d0318e2f60e7002ab20c71a
tomineodegard <tomi0105@stud.kea.dk>
2022-06-17 16:21:44 +0200

    new campgroundmap and lagout

--- src/pages/ShopSubPages/CampgroundForm.jsx
+++ src/pages/ShopSubPages/CampgroundForm.jsx
@@ -3,12 +3,13 @@ import { useState, useContext, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { ShopContext } from '../../contexts/ShopContext';
 import CampgroundsMap from '../../components/selectionMap/CampgroundsMap';
-import CheckboxCard from "../../components/optionCards/CheckboxCard";
+import H2 from "../../components/typography/H2";
+import H3 from '../../components/typography/H3';
+// import CheckboxCard from "../../components/optionCards/CheckboxCard";
 import PrimaryButton from '../../components/buttons/PrimaryButton';
 import ErrorP from '../../components/typography/ErrorP';
 import P from "../../components/typography/P";
-import H4 from "../../components/typography/H4";
-import H2 from "../../components/typography/H2";
+// import H4 from "../../components/typography/H4";
 import axios from 'axios';
 
 function CampgroundForm() {
@@ -99,34 +100,35 @@ function CampgroundForm() {
 
     }
 
-    function setCampsGreenly(value){
-        setShopData((oldData) => {
-            let newData = {...oldData};
-            newData.greenCamping.selected = value;
-            return newData;
-        })
-    }
+    // function setCampsGreenly(value){
+    //     setShopData((oldData) => {
+    //         let newData = {...oldData};
+    //         newData.greenCamping.selected = value;
+    //         return newData;
+    //     })
+    // }
   return (
       <>
-    <H2>Step 3:</H2>
-    <H2 classModifiers="mb-10">Choose your campground</H2>
-    <form className='grid grid-col-1 md:grid-col-2 gap-4'>
-        <div className='col-start-1 md:max-h-96 w-full flex flex-col md:flex-row gap-4 bg-darkmode_black2 p-8'>
-            <CampgroundsMap value={activeCampground?.area} clickFunc={handleMapClick}></CampgroundsMap>
-            <div className='text-shade_darker_white md:w-1/3 break-words'>
-                <H4>Choose your campground</H4>
-                {activeCampground?
-                    <>
-                        <P classModifiers="font-bold mt-5">Avaliable spaces at {activeCampground.area}: <span className='font-medium text-accent_yellow'>{activeCampground.available}</span></P> 
-                    </>: null
-                }
-                
-                
-            </div>    
+      <H2 classModifiers="mb-10 text-5xl">Step 3:</H2>
+        <form className='grid grid-col-1 md:grid-cols-2 gap-4 lg:gap-8'>
+        <div className='flex flex-col border border-white p-6 gap-4'>
+            <H3 classModifiers="text-white">Choose your campground</H3>
+            <P>Click a campground to check for avaliable spaces at your preferred campground. Toilets and hygiene facilites are installed at all campgrounds.</P>
+            </div>
+            <div className='w-full h-full'>
+                <CampgroundsMap value={activeCampground?.area} clickFunc={handleMapClick}></CampgroundsMap>
+                <div className='text-shade_darker_white md:w-1/3 break-words'>
+                    {activeCampground?
+                        <>
+                            <P classModifiers="font-bold mt-5">Avaliable spaces at {activeCampground.area}: <span className='font-medium text-accent_yellow'>{activeCampground.available}</span></P> 
+                        </>: null
+                    }
+            </div>  
+              
         </div>
-        <div className='col-start-1 md:col-start-2'>
+        {/* <div className='col-start-1 md:col-start-2'>
             <CheckboxCard {...shopData.greenCamping} price={shopData.greenCamping.price + " Kr."} value={shopData.greenCamping.selected} setValue={setCampsGreenly}></CheckboxCard>
-        </div>
+        </div> */}
         <div className='col-start-1 md:col-start-2 md:row-start-2 w-full flex flex-row justify-end'>
             {formValid? null : <ErrorP>Please select a Campground with enough free spaces</ErrorP>}
             <PrimaryButton caption="Confirm" action={submit}></PrimaryButton>
