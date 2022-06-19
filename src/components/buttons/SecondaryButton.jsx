fba5115a36069e48e3f76dd1532d943052b9930d
tomineodegard <tomi0105@stud.kea.dk>
2022-06-19 20:48:50 +0200

    some styling changes on buttons

--- src/components/buttons/SecondaryButton.jsx
+++ src/components/buttons/SecondaryButton.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SecondaryButton(props) {
   return (
-    <button className="box-border text-shade_darker_white tracking-wider uppercase
-        border-shade_darker_white border-2 px-5 py-0.5 hover:border-accent_yellow2" 
+    <button className="w-full md:w-fit box-border text-shade_darker_white tracking-wider uppercase
+        border-shade_darker_white border-2 px-5 py-2-5 outline-offset-0 hover:border-accent_yellow2" 
         onClick={props.action}>{props.caption}
     </button>
 
