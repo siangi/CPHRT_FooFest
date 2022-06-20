be441993593c6e6dfb000ecc21bf6adc538c7919
tomineodegard <tomi0105@stud.kea.dk>
2022-06-20 10:46:14 +0200

    changed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { AiFillGithub, AiFillFacebook, AiFillInstagram } from "react-icons/ai";
+import P from "./typography/P";
+import { Link } from "react-router-dom";
+import H2 from "./typography/H2";
+
 
 
 function Footer() {
   return (
     <footer className="p-4 sm:p-6 bg-darkmode_black mt-20">
-      <div className="flex flex-col justify-between lg:flex-row gap-10 lg:gap-0">
-        <a href="/">
-          <img
-            src={process.env.PUBLIC_URL + "/LogoText.svg"}
-            className="mr-3 h-12"
-            alt="Ragnarock Logo"
-          />
-        </a>
+      <div className="flex items-center flex-col justify-between a lg:flex-row gap-10 lg:gap-0">
+      <Link to="/">
+            <img
+              src={process.env.PUBLIC_URL + "/LogoText.svg"}
+              alt=""
+              className="w-52 h-full self-center justify-self-center"
+            />
+          </Link>
+          <Link to="/shop">
+            <P classModifiers="font-displayFont hover:fill-accent_yellow cursor-pointer">Copenhagen, August 8th - 14th 2022</P>
+          </Link>
         <div className="flex">
           <AiFillFacebook
             className="text-white h-10 w-auto hover:fill-accent_yellow cursor-pointer"
@@ -26,18 +33,31 @@ function Footer() {
       </div>
 
 
-    <div className='flex md:justify-end mt-6 sm:justify-start'>
-    </div>
-    <hr className="my-6 border-darkmode_black8 sm:mx-auto lg:my-8" />
-    <div className="sm:flex sm:items-center sm:justify-between my-6">
-        <span className="text-sm text-darkmode_black8 sm:text-center">© 2022 George Nicolae, Simon Gisler & Tomine Ødegård.</span>
-        <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
+
+    <div className='flex flex-col justify-start mt-6'>
+      <hr className="mt-6 border-darkmode_black8 lg:my-8" />
+      <div className="flex mb-3 items-baseline gap-4">
+        <P classModifiers="text-darkmode_black8">© 2022 George Nicolae, Simon Gisler & Tomine Ødegård.</P>
+        <div className="flex mt-3">
           <a
-            href="https://github.com/siangi/CPHRT_FooFest"
+            href="https://github.com/siangi/CPHRT_FooFest" target="_blank" rel="noreferrer"
             className="text-darkmode_black8 hover:text-accent_yellow"
           >
             <AiFillGithub className="w-5 h-5 hover:fill-accent_yellow cursor-poiner" />
           </a>
+          </div>
+        </div>
+
+        <div className="flex items-baseline gap-4">
+        <P classModifiers="text-darkmode_black8">© 2.iteration fith focus on UX, CX and a visual lift by Tomine Ødegård.</P>
+        <div className="flex mt-3">
+          <a
+            href="https://github.com/tomineodegard/ragnarock_festival" target="_blank" rel="noreferrer"
+            className="text-darkmode_black8 hover:text-accent_yellow"
+          >
+            <AiFillGithub className="w-5 h-5 hover:fill-accent_yellow cursor-poiner" />
+          </a>
+          </div>
         </div>
       </div>
     </footer>
