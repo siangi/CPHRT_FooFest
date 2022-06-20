e2596ee59b360d3feabb3b631c4ee8f85fd8b82a
tomineodegard <tomi0105@stud.kea.dk>
2022-06-20 17:16:07 +0200

    fixed padding

--- src/components/Navigaton.jsx
+++ src/components/Navigaton.jsx
@@ -24,7 +24,7 @@ export default function Header() {
     <>
       {isDesktop ? ( 
         // DESKTOP NAVIGATION
-        <nav className="flex justify-between px-8 md:px-10 lg:px-12 items-center">
+        <nav className="flex justify-between px-8 md:px-10 lg:px-12 py-8 items-center">
           <Link to="/">
             <img
               src={process.env.PUBLIC_URL + "/LogoText.svg"}
